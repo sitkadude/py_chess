7ff86821462467eb2d2e9d259536f2809a26193d
Lukas Jorgensen <lukas.r.jorgensen@gmail.com>
2019-12-16 20:26:41 -0500

    first commit, rough draft of board

---
+++ chessboard.py
@@ -0,0 +1,57 @@
+# --- PyChess --- #
+ROOK = "RK"
+PAWN = "PN"
+KNIGHT = "KT"
+BISHOP = "BP"
+QUEEN = "QN"
+KING = "KG"
+STRING = "  "
+board = []
+
+# --- Create Chessboard --- #
+for i in range(8):
+    row = ["  " for i in range(8)]
+    board.append(row)
+
+# --- Slot Assignment --- #
+board[0][0] = ROOK
+board[0][7] = ROOK
+board[7][0] = ROOK
+board[7][7] = ROOK
+
+board[1][0] = PAWN
+board[1][1] = PAWN
+board[1][2] = PAWN
+board[1][3] = PAWN
+board[1][4] = PAWN
+board[1][5] = PAWN
+board[1][6] = PAWN
+board[1][7] = PAWN
+board[6][0] = PAWN
+board[6][1] = PAWN
+board[6][2] = PAWN
+board[6][3] = PAWN
+board[6][4] = PAWN
+board[6][5] = PAWN
+board[6][6] = PAWN
+board[6][7] = PAWN
+
+board[7][1] = KNIGHT
+board[7][6] = KNIGHT
+board[0][1] = KNIGHT
+board[0][6] = KNIGHT
+
+board[7][2] = BISHOP
+board[7][5] = BISHOP
+board[0][2] = BISHOP
+board[0][5] = BISHOP
+
+board[0][3] = QUEEN
+board[7][3] = QUEEN
+
+board[0][4] = KING
+board[7][4] = KING
+
+# --- Display Board --- #
+for row in board:
+    print(row)
